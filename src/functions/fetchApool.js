842926bb34dda033a5aaab4174a20a164d977bb5
90yearsoldcoder <minty@bu.edu>
2024-03-24 22:51:51 -0400

    add fetch pokemon pool API

---
+++ src/functions/fetchApool.js
@@ -0,0 +1,40 @@
+import { APIsetting } from "../settings/APIsetting";
+
+function uniqueRandomNumbers(k, N) {
+  let nums = new Set();
+  while (nums.size < k) {
+    let num = Math.floor(Math.random() * N) + 1;
+    nums.add(num);
+  }
+  return Array.from(nums);
+}
+
+async function getPokemonInfo({ id, shiny }) {
+  let url = `${APIsetting.PokemonURL}/${id}`;
+  //console.log(url);
+  const res = await fetch(url);
+  const { name, sprites } = await res.json();
+  const img = sprites[shiny ? "front_shiny" : "front_default"];
+  return { name, img };
+}
+
+async function fetchApool({ poolSize, setLoaded }) {
+  const randomIntArray = uniqueRandomNumbers(poolSize, APIsetting.MAXID);
+  const poolList = [];
+
+  for (let num of randomIntArray)
+    poolList.push({ id: num, shiny: APIsetting.shiny });
+
+  const promises = poolList.map((item) =>
+    getPokemonInfo(item).then((data) => {
+      setLoaded((val) => val + 1);
+      return data;
+    })
+  );
+
+  const responses = await Promise.all(promises);
+
+  return responses;
+}
+
+export default fetchApool;
