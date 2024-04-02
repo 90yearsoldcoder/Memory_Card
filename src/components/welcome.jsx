f30daa19e5b38a58e7e6ae5407f262a2b2c783dc
90yearsoldcoder <minty@bu.edu>
2024-04-02 16:39:57 -0400

    add style to welcome board

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,42 +1,63 @@
 import PropTypes from "prop-types";
 import { gameSettings } from "../settings/gameSettings";
+import "../styles/welcome.css";
+import { useState } from "react";
 
-const Welcome = ({ setPage, setPoolSize, setnumPokePerRound }) => {
+const Welcome = ({ poolSize, setPage, setPoolSize, setnumPokePerRound }) => {
   function handleDiffClick(e) {
-    //console.log(e.target.id);
+    //console.log("button clicked " + e.target.id);
     setPoolSize(gameSettings["poolSize"][e.target.id]);
     setnumPokePerRound(gameSettings["pokePerRound"][e.target.id]);
   }
   function handleStartClick() {
     setPage("Game");
   }
+  function ButtonStyle(ind) {
+    //console.log(ind);
+    //console.log(buttonSelected);
+    let diff = "Medium";
+    for (let key of Object.keys(gameSettings["poolSize"]))
+      if (poolSize == gameSettings["poolSize"][key]) diff = key;
+
+    if (ind === diff) return "diff-button btnSelected";
+    return "diff-button";
+  }
 
   return (
-    <>
-      <p>Welcome</p>
+    <div className="welcome-container">
+      <div className="title">Welcome</div>
       <div className="diff-select-container">
-        <button className="diff-button" id="Easy" onClick={handleDiffClick}>
+        <button
+          className={ButtonStyle("Easy")}
+          id="Easy"
+          onClick={handleDiffClick}
+        >
           Easy
         </button>
-        <button className="diff-button" id="Medium" onClick={handleDiffClick}>
+        <button
+          className={ButtonStyle("Medium")}
+          id="Medium"
+          onClick={handleDiffClick}
+        >
           Medium
         </button>
         <button
-          className="diff-button"
-          id="Difficult"
+          id="Hard"
+          className={ButtonStyle("Hard")}
           onClick={handleDiffClick}
         >
-          Difficult
+          Hard
         </button>
       </div>
       <button className="start-button" id="Start" onClick={handleStartClick}>
         Start
       </button>
-    </>
+    </div>
   );
 };
 
 Welcome.propTypes = {
+  poolSize: PropTypes.number.isRequired,
   setPage: PropTypes.func.isRequired,
   setPoolSize: PropTypes.func.isRequired,
   setnumPokePerRound: PropTypes.func.isRequired,
